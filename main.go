818a75f8da7316545ae0bae96f1c6eb2ec5f9bf3
wangxso <wangxs0102@gmail.com>
2023-12-08 19:51:40 +0800

    resume main.go

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"flag"
 
-	"net/http"
 	_ "net/http/pprof"
 
+	"github.com/wangxso/backuptool/auth"
 	clousync "github.com/wangxso/backuptool/cloudsync"
 	"github.com/wangxso/backuptool/config"
 	"github.com/wangxso/backuptool/db"
@@ -30,21 +30,17 @@ func init() {
 
 func main() {
 	defer handler.HandlerGlobalErrors()
-	go func() {
-		http.ListenAndServe("localhost:6060", nil)
-	}()
 	flag.Parse()
 	config.LoadConfig(DEFAULT_CONFIG_PATH)
 	db.LoadRedis()
-	clousync.SyncFolder()
-	// // 登录模式
-	// if authFlag {
-	// 	auth.Login()
-	// 	return
-	// }
-	// // 同步模式
-	// if syncFlag {
-	// 	clousync.SyncFolder()
-	// 	return
-	// }
+	// 登录模式
+	if authFlag {
+		auth.Login()
+		return
+	}
+	// 同步模式
+	if syncFlag {
+		clousync.SyncFolder()
+		return
+	}
 }
